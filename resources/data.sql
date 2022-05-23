1915bd1c4a99476971e115d5a94344fd269eda4f
Sean Merz <72666129+smmerz56@users.noreply.github.com>
2022-05-22 20:56:40 -0700

    Add files via upload

---
+++ resources/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, 'Learn Springboot', false, sysdate(), 'in28Minutes');
+insert into TODO
+values(10002, 'Learn Angular JS', false, sysdate(), 'in28Minutes');
+insert into TODO
+values(10003, 'Learn Java', false, sysdate(), 'in28Minutes');
